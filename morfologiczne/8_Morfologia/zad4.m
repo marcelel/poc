34ada0447d8d615cb2b8d342ebe919190c723b73
marcelel <marcel.lekston@gmail.com>
2017-11-27 01:18:29 +0100

    morfologiczne poprawione

--- morfologiczne/8_Morfologia/zad4.m
+++ morfologiczne/8_Morfologia/zad4.m
@@ -2,7 +2,8 @@ close all; clearvars; clc;
 
 Calculator = imread('calculator.bmp');
 
-%%
+%% 
+%2
 Calculator_erode = imerode(Calculator, ones(1,71));
 Calculator_reconstruct = imreconstruct(Calculator_erode, Calculator);
 
@@ -24,13 +25,13 @@ subplot(2,2,4); imshow(Calculator_tophat); title('Top-hat');
 
 %%
 figure;
+Calculator_reconstruct_vertical = imreconstruct(imerode(Calculator_diff, ones(1,11)), Calculator_diff);
 subplot(1,2,1); imshow(Calculator_diff); title('Roznica');
-subplot(1,2,2); imshow(imreconstruct(imerode(Calculator_diff, ones(1,11)), Calculator_diff)); title('Odblaski pionowe');
+subplot(1,2,2); imshow(Calculator_reconstruct_vertical); title('Odblaski pionowe');
 
 %%
-% to chyba zle
-Calculator_dilate = imdilate(Calculator_diff, ones(1,21));
+Calculator_dilate = imdilate(Calculator_reconstruct_vertical, ones(1,21));
 figure;
 subplot(1,3,1); imshow(Calculator); title('Original');
 subplot(1,3,2); imshow(Calculator_dilate); title('Dylatacja');
-subplot(1,3,3); imshow(imreconstruct(min(Calculator_dilate, Calculator_reconstruct), Calculator_reconstruct));
+subplot(1,3,3); imshow(imreconstruct(min(Calculator_dilate, Calculator_diff), Calculator_diff)); title('Rekonstrukcja');
